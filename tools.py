2807de2b028837798a344881c4cbecae5e84a089
Jiri Hubacek <hubacekjirka@gmail.com>
2020-01-27 15:57:09 +1100

    WIP - working prototype

--- tools.py
+++ tools.py
@@ -7,21 +7,17 @@ s3 = boto3.client(
    aws_secret_access_key=app.config['S3_SECRET']
 )
 
-
-def upload_file_to_s3(file, bucket_name, acl="public-read"):
+def upload_file_to_s3(file, bucket_name):
 
     try:
-
         s3.upload_fileobj(
             file,
             bucket_name,
             file.filename,
             ExtraArgs={
-                "ACL": acl,
                 "ContentType": file.content_type
             }
         )
-
     except Exception as e:
         print("Something Happened: ", e)
         return e
